0509033f15f92bc3374560831bc24c299ecbb901
Lisa Fink <lisadfink@gmail.com>
2022-08-10 11:33:08 -0400

    Added divs for timer, flag count, and reset button

--- src/components/TopBoard.js
+++ src/components/TopBoard.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Timer from './Timer';
 
+import '../styles/topboard.css';
+
 const TopBoard = (props) => {
-  const { seconds, setSeconds, isActive, formattedTime } = props;
+  const { seconds, setSeconds, isActive, formattedTime, flags } = props;
   return (
-    <div>
-      top of the board
-      <Timer
-        seconds={seconds}
-        setSeconds={setSeconds}
-        isActive={isActive}
-        formattedTime={formattedTime}
-      />
+    <div className="top-board">
+      <div className="top-container flags">
+        <div className="top-text-box">{flags}</div>
+      </div>
+      <div className="top-container">
+        <button className="reset-button">Cool</button>
+      </div>
+      <div className="top-container timer">
+        <div className="top-text-box">
+          <Timer
+            seconds={seconds}
+            setSeconds={setSeconds}
+            isActive={isActive}
+            formattedTime={formattedTime}
+          />
+        </div>
+      </div>
     </div>
   );
 };
