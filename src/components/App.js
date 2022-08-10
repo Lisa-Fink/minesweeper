2f3c291cc49d071bb59f743c015816a1d1cd1189
Lisa Fink <lisadfink@gmail.com>
2022-08-10 12:05:46 -0400

    Added reset function and adjusted grid to indexes

--- src/components/App.js
+++ src/components/App.js
@@ -8,9 +8,8 @@ import TopBoard from './TopBoard';
 function App() {
   const blank_board = new Array(81).fill(0);
 
-  // 81 tiles - pick 10 out of 81
   const [flags, setFlags] = useState(0);
-  const [board, setBoard] = useState([...blank_board]);
+  const [board, setBoard] = useState([]);
 
   // timer states
   const [isActive, setIsActive] = useState(false);
@@ -28,23 +27,29 @@ function App() {
     }
   }, []);
 
+  const resetGame = () => {
+    setSeconds(0);
+    setIsActive(false);
+    endOfGame.current = false;
+    setFlags(0);
+    createMines();
+  };
+
   const createMines = () => {
-    // creates an array of nums from 1-81
-    let grid = [...Array(82).keys()].slice(1);
-    let board_copy = [...board];
+    // creates an array of nums from 0-80
+    let grid = [...Array(81).keys()];
+    let board_copy = [...blank_board];
     for (let i = 0; i < 10; i++) {
-      // adds the number at grid index mine as a mine
-      // grid[mine] is 2 then the index the board is 1
-      // mine will be an index in grid
+      // adds the index at grid index mine as a mine
       let mine = Math.floor(Math.random() * grid.length);
       board_copy = [
         ...board_copy.slice(0, grid[mine]),
         'x',
         ...board_copy.slice(grid[mine] + 1),
       ];
+      console.log('set mine', grid[mine] + 1);
       // removes the mine that was just added
       grid.splice(mine, 1);
-      console.log('set mine', grid[mine]);
     }
     mineBoard.current = [...board_copy];
     setBoard(board_copy);
@@ -77,6 +82,7 @@ function App() {
           isActive={isActive}
           formattedTime={formattedTime}
           flags={flags}
+          resetGame={resetGame}
         />
         <Board
           board={board}
