acb3019f3e0beb8d0fd58e5b5a0afa197527426d
Lisa Fink <lisadfink@gmail.com>
2022-08-10 12:04:39 -0400

    Added reset button

--- src/components/TopBoard.js
+++ src/components/TopBoard.js
@@ -4,14 +4,17 @@ import Timer from './Timer';
 import '../styles/topboard.css';
 
 const TopBoard = (props) => {
-  const { seconds, setSeconds, isActive, formattedTime, flags } = props;
+  const { seconds, setSeconds, isActive, formattedTime, flags, resetGame } =
+    props;
   return (
     <div className="top-board">
       <div className="top-container flags">
         <div className="top-text-box">{flags}</div>
       </div>
       <div className="top-container">
-        <button className="reset-button">Cool</button>
+        <button className="reset-button" onClick={resetGame}>
+          Cool
+        </button>
       </div>
       <div className="top-container timer">
         <div className="top-text-box">
